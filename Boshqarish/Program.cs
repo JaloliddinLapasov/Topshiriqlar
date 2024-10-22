15d9825ddc28188e1ef9e1173a241e5f3a5769fb
Jaloliddin-lapasov_ <jaloliddin630@gmail.com>
2024-10-22 17:12:27 +0500

    darslar

---
+++ Boshqarish/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+class InventarizatsiyaBoshqaruvi{
+    static void Main(){
+        InventarizatsiyaTizimi();
+    }
+
+    static void InventarizatsiyaTizimi()
+    {
+        string[] mahsulotlar = new string[100];
+        int[] miqdorlar = new int[100];
+        int mahsulotSoni = 0; 
+        string davomEtish;
+
+        do{
+            Console.WriteLine("\nInventarizatsiya boshqaruv tizimi:");
+            Console.WriteLine("1.Yangi mahsulot qo'shish");
+            Console.WriteLine("2.Mahsulot miqdorini yangilash");
+            Console.WriteLine("3.Mahsulotni olib tashlash");
+            Console.WriteLine("4.Inventarizatsiya ro'yxatini ko'rsatish");
+            Console.Write("Amalni tanlang (1-4): ");
+            int amal = Convert.ToInt32(Console.ReadLine());
+            switch (amal){
+                case 1: 
+                    if (mahsulotSoni < 100) {
+                        Console.Write("Mahsulot nomini kiriting: ");
+                        mahsulotlar[mahsulotSoni] = Console.ReadLine();
+                        Console.Write("Mahsulot miqdorini kiriting: ");
+                        miqdorlar[mahsulotSoni] = Convert.ToInt32(Console.ReadLine());
+                        mahsulotSoni++;
+                        Console.WriteLine("Mahsulot qo'shildi.");
+                    }
+                    else{
+                        Console.WriteLine("Mahsulotlar ro'yxati to'liq!");
+                    }
+                    break;
+
+                case 2: 
+                    Console.Write("Yangilamoqchi bo'lgan mahsulot raqamini kiriting (1 - {0}): ", mahsulotSoni);
+                    int yangilashRaqami = Convert.ToInt32(Console.ReadLine()) - 1;
+
+                    if (yangilashRaqami >= 0 && yangilashRaqami < mahsulotSoni)
+                    {
+                        Console.Write("Yangi miqdorni kiriting: ");
+                        miqdorlar[yangilashRaqami] = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Mahsulot miqdori yangilandi.");
+                    }
+                    else{
+                        Console.WriteLine("Noto'g'ri mahsulot raqami.");
+                    }
+                    break;
+
+                case 3: 
+                    Console.Write("Olib tashlamoqchi bo'lgan mahsulot raqamini kiriting (1 - {0}): ", mahsulotSoni);
+                    int olibTashlashRaqami = Convert.ToInt32(Console.ReadLine()) - 1;
+
+                    if (olibTashlashRaqami >= 0 && olibTashlashRaqami < mahsulotSoni)
+                    {
+                        for (int i = olibTashlashRaqami; i < mahsulotSoni - 1; i++)
+                        {
+                            mahsulotlar[i] = mahsulotlar[i + 1];
+                            miqdorlar[i] = miqdorlar[i + 1];
+                        }
+                        mahsulotSoni--;
+                        Console.WriteLine("Mahsulot olib tashlandi.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Noto'g'ri mahsulot raqami.");
+                    }
+                    break;
+
+                case 4: 
+                    Console.WriteLine("\nInventarizatsiya ro'yxati:");
+                    for (int i = 0; i < mahsulotSoni; i++)
+                    {
+                        Console.WriteLine($"{i + 1}. {mahsulotlar[i]} - {miqdorlar[i]} dona");
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("Noto'g'ri amal tanlandi.");
+                    break;
+            }
+
+            Console.Write("Yana amal bajarishni xohlaysizmi? (h/y): ");
+            davomEtish = Console.ReadLine().ToLower();
+
+        } while (davomEtish == "h");
+
+        Console.WriteLine("Dastur tugadi.");
+    }
+}
---
+++ Bron_kino/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+class Film_bileti_bron
+{
+    static void Main()
+    {
+        BronQilishTizimi();
+    }
+
+    static void BronQilishTizimi(){
+        int orindiqlarSoni = 10;
+        string[] orindiqlar = new string[orindiqlarSoni];
+        bool[] mavjudlik = new bool[orindiqlarSoni];
+        for (int i = 0; i < orindiqlarSoni; i++){
+            orindiqlar[i] = $"O'rindiq {i + 1}";
+            mavjudlik[i] = true; 
+        }
+        int tanlov = 0;
+
+        do
+        {
+            Console.WriteLine("\nMavjud o'rindiqlar:");
+            for (int i = 0; i < orindiqlarSoni; i++){
+                if (mavjudlik[i])
+                {
+                Console.WriteLine($"{orindiqlar[i]} - Mavjud");
+                }
+                else
+                {
+                Console.WriteLine($"{orindiqlar[i]} - Band");
+                }
+            }
+
+            Console.WriteLine("\nQaysi o'rindiqni bron qilishni xohlaysiz? (1-10) yoki tizimdan chiqish uchun 0:");
+            if (int.TryParse(Console.ReadLine(), out tanlov) && (tanlov >= 0 && tanlov <= orindiqlarSoni))
+            {
+                if (tanlov == 0)
+                {
+                    Console.WriteLine("Dasturdan chiqyapsiz...");
+                    break;
+                }
+
+                int orindiqIndex = tanlov - 1;
+                if (mavjudlik[orindiqIndex])
+                {
+                    mavjudlik[orindiqIndex] = false; 
+                    Console.WriteLine($"{orindiqlar[orindiqIndex]} bron qilindi.");
+                }
+                else
+                {
+                    Console.WriteLine($"{orindiqlar[orindiqIndex]} allaqachon band qilingan.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Noto'g'ri tanlov. Iltimos, 1 dan 10 gacha yoki 0 ni kiriting.");
+            }
+
+        } while (true); 
+    }
+}
